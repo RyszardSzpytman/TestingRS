4fa18fde4d1bdc243f007a31f376bb076fb6b550
RyszardSzpytman <66265356+RyszardSzpytman@users.noreply.github.com>
2020-09-16 21:24:04 +0200

    Add files via upload

--- MyStoreOpenSteps.java
+++ MyStoreOpenSteps.java
@@ -1,15 +1,58 @@
 package steps;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import org.openqa.selenium.WebDriver;
-import pages.MyStoreOpenPage;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.*;
 
 public class MyStoreOpenSteps {
     private WebDriver driver;
 
     @Given("^open mystore with google$")
     public void open_mystore_with_google() {
-        MyStoreOpenPage openPage = new MyStoreOpenPage(driver);
+
+        System.setProperty("webdriver.chrome.driver",
+                "src/main/resources/drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://prod-kurs.coderslab.pl");
 
     }
+
+    @When("^sign in user$")
+    public void sign_in_user() {
+        MyStoreLoginPage loginPage = new MyStoreLoginPage(driver);
+        loginPage.loginAs("jankowal5ki@wp.pl", "janko1");
+    }
+
+    @Then("^user go to address section$")
+    public void user_go_to_address_section(){
+        MyStoreAddressSectionPage addressSectionPage=
+        new MyStoreAddressSectionPage(driver);
+        addressSectionPage.addressSection();
+        }
+
+    @And("^fill the address form (.*), (.*), (.*), (.*)$")
+    public void fill_the_address_form(
+        String address,String city,String zip,String country){
+        MyStoreUserAddressPage addressPage = new MyStoreUserAddressPage(driver);
+        addressPage.addAddress((String) address, (String) city, (String) zip, (String) country);
+
+        }
+
+    @When("^check address$")
+    public void check_address(){
+        MyStoreCheckAddressPage checkAddressPage = new MyStoreCheckAddressPage(driver);
+        checkAddressPage.checkAddress();
+        }
+
+    @And("^close browser$")
+    public void close_browser(){
+        MyStoreClosePage closePage = new MyStoreClosePage(driver);
+        closePage.pageQuit();
+
+        }
 }
